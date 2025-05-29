de6d6ac369b8d6b66069d82fb2e149d9001bce21
fluxiti <164828519+fluxiti@users.noreply.github.com>
2025-05-28 23:05:16 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+let songs = [
+    {
+name: 'Usher - There Goes My Baby (Official Music Video)',
+path: 'Website pics\Usher - There Goes My Baby (Official Music Video).mp3',
+artist: 'artist 1',
+cover: 'Website pics\Img6.jpg'
+    }
+]
